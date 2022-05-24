1826d8923452416230b1f405ea916e4c4b2b85e2
"Manoj KUMAR Bangaru" <"bmanoj.ba@gmail.com">
2022-05-24 11:11:48 +0530

    Constructor

--- contracts/GetterSetter.sol
+++ contracts/GetterSetter.sol
@@ -2,13 +2,8 @@
 pragma solidity >=0.4.0 <0.9.0;
 
 contract Getter{
-    uint age=20; 
-    //If you are declaring a variable as 'public', there is no need of writing getter function
-    function getter() public view returns(uint){ //'View' keyword is used since we are not changing state variable.
-    
+    uint age=20;
+    function getter() public view returns(uint){
         return age;
     } 
-    function setter() public{
-        age++;
-    }
 }
